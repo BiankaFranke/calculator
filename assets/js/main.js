499ac5ea48a6f49eddd4bdafc93fe5c3185fefae
“BiankaFranke” <bianka_franke@hotmail.de>
2022-07-03 09:56:28 +0200

    History added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,25 @@
+// JS Check
 console.log('it works');
 
+// Declaration
 let calculator = document.getElementById('result');
+let history = document.getElementById("history");
 let buttons = Array.from(document.getElementsByClassName('button'));
-
-console.log(buttons);
 
 buttons.map(button => {
     button.addEventListener('click', (event) => {
         switch(event.target.innerText) {
             case 'c':
                 calculator.innerText = '0';
+                history.innerText = '';
                 break;
             case '=':
                 calculator.innerText = eval(calculator.innerText);
+                history.innerText = eval(calculator.innerText);
                 break;
             default:
                 calculator.innerText += event.target.innerText;
+                history.innerText += event.target.innerText;
         } 
     });
 });
